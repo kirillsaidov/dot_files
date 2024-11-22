f8664176d6196d459e3d40b9e115d0954bd1c6e0
Kirill Saidov <kirillsaidovd@yahoo.com>
2024-11-22 12:41:53 +0500

    update

--- system_install.sh
+++ system_install.sh
@@ -3,12 +3,16 @@
 # setup variables
 HELP=${HELP:-0}
 SKIP_OLLAMA=${SKIP_OLLAMA:-0}
+SKIP_DOCKER=${SKIP_DOCKER:-0}
+SKIP_MONGO=${SKIP_MONGO:-0}
 
 # usage
 if [ "$HELP" -eq 1 ]; then
     cat <<EOF
 USAGE: system_install.sh
     SKIP_OLLAMA   {0, 1} Do not install ollama. Default=0.
+    SKIP_DOCKER   {0, 1} Do not install docker. Default=0.
+    SKIP_MONGO    {0, 1} Do not install mongo. Default=0.
     HELP          {0, 1} This help manual. Default=0.
 EOF
     exit 0
@@ -28,8 +32,8 @@ sudo apt install -y audacity tilix
 
 # --- vscode
 wget -O vscode-linux.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-sudo apt install vscode-linux.deb
-rm vscode-linux.dev
+sudo apt install ./vscode-linux.deb
+rm vscode-linux.deb
 
 # --- sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
@@ -37,16 +41,43 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 sudo apt update && sudo apt install -y sublime-text
 
 # --- docker
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $USER
+if [ "$SKIP_DOCKER" -eq 0 ]; then
+    # install docker
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo usermod -aG docker $USER
+    
+    # configure docker logs size
+    sudo tee /etc/docker/daemon.json > /dev/null <<EOF
+{
+    "log-driver": "json-file",
+    "log-opts": {
+        "max-size": "10m",
+        "max-file": "3"
+    }
+}
+EOF
+fi
+
+# --- mongo
+if [ "$SKIP_MONGO" -eq 0 ]; then
+    # install mongo with docker
+    sudo docker pull mongodb/mongodb-community-server:latest
+    sudo docker run --name mongodb -p 27017:27017 -d mongodb/mongodb-community-server:latest
+    docker update --restart unless-stopped $(docker ps -aqf "name=mongodb")
+
+    # install mongo compass
+    wget -O mongo-compass.deb "https://downloads.mongodb.com/compass/mongodb-compass_1.44.7_amd64.deb"
+    sudo apt install ./mongo-compass.deb
+    rm mongo-compass.deb
+fi
 
 # --- ollama
 if [ "$SKIP_OLLAMA" -eq 0 ]; then
